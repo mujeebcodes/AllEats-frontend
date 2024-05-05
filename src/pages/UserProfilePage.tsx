bc2a418749ffdba7ef40b01a24d8608573406589
mujeebcodes <mailmujeeb@icloud.com>
2024-05-05 20:17:33 +0100

    user-profile page  with prepolutated fields

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,6 +1,23 @@
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-  return <UserProfileForm />;
+  const { currentUser, isLoading: isLoadingUser } = useGetMyUser();
+  const { updateUser, isLoading: isUpdatingUser } = useUpdateMyUser();
+
+  if (isLoadingUser) {
+    return <span>Loading ... </span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdatingUser}
+    />
+  );
 };
 export default UserProfilePage;
